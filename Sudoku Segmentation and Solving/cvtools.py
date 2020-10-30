f284e3fbb4774c107706fa71b56a6e2be8e530ce
Shaibal Karmakar <68264185+pecacio@users.noreply.github.com>
2020-10-30 11:05:22 +0530

    Update cvtools.py
    These functions are repeatedly used in the programs to visualize the intermediate steps during debugging

--- Sudoku Segmentation and Solving/cvtools.py
+++ Sudoku Segmentation and Solving/cvtools.py
@@ -1,26 +1,23 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
-def show(img,name='img'):
+
+#Functions to view images using opencv
+
+def show(img,name='img'):#Showed in original size
     while(1):
         cv2.imshow(name,img)
         if cv2.waitKey(1) & 0xFF==ord('q'):
             break
     cv2.destroyAllWindows()
-def show2(img,name='img'):
+    
+def show2(img,name='img'):#Image is fit to Window Size
     cv2.namedWindow(name,cv2.WINDOW_NORMAL)
     while(1):
         cv2.imshow(name,img)
         if cv2.waitKey(1) & 0xFF==ord('q'):
             break
     cv2.destroyAllWindows()
-def save(img,name):
+    
+def save(img,name):#Function to save a image
     img=img.astype(np.uint8)
-    cv2.imwrite(name,img)   
-def hist_norm(gray):
-    equ=cv2.equalizeHist(gray)
-    return equ
-def clahe_norm(gray):
-    clahe=cv2.createCLAHE(clipLimit=2.0,tileGridSize=(8,8))
-    cl1=clahe.apply(gray)
-    return cl1
+    cv2.imwrite(name,img)
